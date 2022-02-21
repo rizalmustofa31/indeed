ad18c1a6587005f2813aa1aa89c151f282ea672c
Rizal Mustofa <mustofarizal1994@gmail.com>
2022-02-21 16:27:24 +0700

    company link

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.indeed.com/jobs?'
-
+site = 'https://www.indeed.com/'
 params = {
     'q': 'Python Developer',
     'l': 'New York State',
@@ -47,6 +47,39 @@ def get_total_pages():
     print(total)
     return total
 
+def get_all_items():
+    params = {
+        'q': 'Python Developer',
+        'l': 'New York State',
+        'vjk': '71acc67a281f6038'
+    }
+    res = requests.get(url,params=params, headers=headers)
+
+    with open('temp/res.html', 'w+') as outfile:
+        outfile.write(res.text)
+        outfile.close()
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    #scraping prosses
+    contents = soup.find_all('table','jobCard_mainContent big6_visualChanges')
+
+    # pick item
+    # * title
+    # * company name
+    # * company link
+    # * company address
+
+    for item in contents:
+        title = item.find('h2', 'jobTitle').text
+        company = item.find('span', 'companyName')
+        company_name = company.text
+        try:
+            company_link = site + company.find('a')['href']
+        except:
+            company_link = 'Link is not available'
+
+        print(company_link)
+
 
 if __name__ == '__main__':
-    get_total_pages()
+    get_all_items()
